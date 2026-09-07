54d74418aa419a11f5f5befc908bd6979157485f
23da2-0899-Shahama <23da2-0899@sltc.ac.lk>
2026-09-07 13:16:56 +0530

    Created project structure

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("========================================");
+        System.out.println(" MINI HOSPITAL EMERGENCY MANAGEMENT SYSTEM");
+        System.out.println("========================================");
+        System.out.println("System started successfully!");
+    }
+}
